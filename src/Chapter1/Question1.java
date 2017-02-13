028aa0dc3108241357501eb3fc791ec7bf620f00
Ashraf <ashcoding@outlook.com>
2017-02-13 19:25:15 +0800

    Initial Commit

---
+++ src/Chapter1/Question1.java
@@ -0,0 +1,13 @@
+package Chapter1;
+
+public class Question1 {
+
+	public Question1() {
+		
+	}
+	
+	public String runCode1() {
+		return "Code 1";
+	}
+	
+}
---
+++ src/Entry.java
@@ -0,0 +1,9 @@
+import Chapter1.Question1;
+
+public class Entry {
+	
+	public static void main(String[] args) {
+		System.out.println(new Question1().runCode1());
+	}
+
+}
