3b89bad6cd5bbbbad43b061902372063305b39c3
Ashraf <ashcoding@outlook.com>
2017-02-13 20:50:16 +0800

    Completed problem 1.1

--- src/Chapter1/Question1.java
+++ src/Chapter1/Question1.java
@@ -3,11 +3,37 @@ package Chapter1;
 public class Question1 {
 
 	public Question1() {
+
+	}
+
+	// 1.1 Is Unique
+	public void runCode1() {
+		String a = "cates";
+		String b = "doges";
+		String c = "sneks";
+		System.out.println(a + " is " + (isUnique(a) ? "unique" : "not unique"));
+		System.out.println(b + " is " + (isUnique(b) ? "unique" : "not unique"));
+		System.out.println(c + " is " + (isUnique(c) ? "unique" : "not unique"));
+	}
+
+	public boolean isUnique(String str) {
+		// Total number of unique char in ASCII is 128
+		// Therefore, if string is more than 128, it isn't unique
+		if (str.length() > 128) {
+			return false;
+		}
 		
+		boolean[] charArray = new boolean[128];
+		// Go through each char in the String
+		for (int i = 0; i < str.length(); i++) {
+			int charVal = str.charAt(i);
+			// Char is found
+			if (charArray[charVal]) {
+				return false;
+			}
+			charArray[charVal] = true;	
+		}
+		return true;
 	}
-	
-	public String runCode1() {
-		return "Code 1";
-	}
-	
+
 }
--- src/Entry.java
+++ src/Entry.java
@@ -3,7 +3,7 @@ import Chapter1.Question1;
 public class Entry {
 	
 	public static void main(String[] args) {
-		System.out.println(new Question1().runCode1());
+		new Question1().runCode1();
 	}
 
 }
